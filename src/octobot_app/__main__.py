ae8ab9cdf5949d804c74cf7d1f170f8c47f96a54
Herklos <herklos.dev@protonmail.com>
2021-08-13 23:30:13 +0200

    Test dirty patch

--- src/octobot_app/__main__.py
+++ src/octobot_app/__main__.py
@@ -1,9 +1,25 @@
+import _ctypes
 import time
 
-import octobot.cli as cli
-from octobot_app.app import main
 
-if __name__ == '__main__':
+def fix_lipthread():
+    print("fix_lipthread")
+    origin_dlopen = _ctypes.dlopen
+
+    def new_dlopen(name, flag, *args, **kwargs):
+        print("new_dlopen")
+        if name == "libpthread.so.0":
+            print("libpthread.so.0")
+            return
+        return origin_dlopen(name, flag, *args, **kwargs)
+
+    _ctypes.dlopen = new_dlopen
+
+
+def main():
+    import octobot.cli as cli
+    from octobot_app.app import main
+
     # start OctoBot
     bot_process = cli.start_background_octobot_with_args(in_subprocess=True)
 
@@ -13,3 +29,8 @@ if __name__ == '__main__':
 
     # start App
     main().main_loop()
+
+
+if __name__ == '__main__':
+    fix_lipthread()
+    main()
